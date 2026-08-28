fce1dda602b06b5749fc9b729c34f1aa5df69304
Samuel Bech Gayne <sgay@itu.dk>
2026-08-28 14:59:24 +0200

    Making it a static class
    sgay

--- Bison.CLI/Program.cs
+++ Bison.CLI/Program.cs
@@ -1,26 +1,58 @@
 ﻿
-try{
-string Auther, Observation, timestamp;
-string filePath = Path.GetFullPath("bison_observe_cli_db.csv");
-//string filePath = @"C:\Users\Bruger\Desktop\ITU\ToBeRenamed\Project\Bison\Bison.CLI\bison_observe_cli_db.csv";
-using(StreamReader reader = new StreamReader(File.OpenRead(filePath)))
-    {
-        string line;
-        string waste = reader.ReadLine();
-        while(!reader.EndOfStream)
-            {
-                line = reader.ReadLine();
+using System;
 
-                string[] Observe = line.Split(","); 
-                Auther = Observe[0];
-                Observation = Observe[1];
-                timestamp = Observe[2];
 
-                Console.WriteLine(Auther + " & " + Observation + " & " + timestamp);
-
-            }
-    }
-    } catch(Exception e)
+namespace Bison.CLI
 {
-    Console.WriteLine("EEEEEEEEEEEEEEEE");
+static class program{
+static string Auther, Observation;
+static string filePath = Path.GetFullPath("bison_observe_cli_db.csv");
+
+//string filePath = @"C:\Users\Bruger\Desktop\ITU\ToBeRenamed\Project\Bison\Bison.CLI\bison_observe_cli_db.csv";
+static int Main(string[] args)
+        {
+        string line = args[0];
+        if(line == "read")
+            {
+                read();
+            }
+    return 0;
+    }
+
+static void read()
+        {
+            
+            try{
+
+
+            string Auther, Observation;
+            string filePath = Path.GetFullPath("bison_observe_cli_db.csv");
+
+            //string filePath = @"C:\Users\Bruger\Desktop\ITU\ToBeRenamed\Project\Bison\Bison.CLI\bison_observe_cli_db.csv";
+            using(StreamReader reader = new StreamReader(File.OpenRead(filePath)))
+                {
+                    string line;
+                    string waste = reader.ReadLine();
+                    while(!reader.EndOfStream)
+                        {
+                            line = reader.ReadLine();
+
+                            string[] Observe = line.Split(","); 
+                            Auther = Observe[0];
+                            Observation = Observe[1];
+                            var observeTime = DateTimeOffset.FromUnixTimeSeconds(long.Parse(Observe[2])).ToLocalTime();
+                            var timeFormatted = observeTime.ToString("dd'/'MM'/'yy HH:mm:ss");
+                            Console.WriteLine(Auther + " @ " + timeFormatted + ": " + Observation);
+
+                        }
+                }
+                } catch(Exception e)
+            {
+                Console.WriteLine("EEEEEEEEEEEEEEEE");
+            }
+
+
+        }
+    }
 }
+        
