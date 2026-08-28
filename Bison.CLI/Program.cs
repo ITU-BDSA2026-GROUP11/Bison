8c105fd550af965cf11d44b368efaa88f5d41e0e
Samuel Bech Gayne <sgay@itu.dk>
2026-08-28 13:39:21 +0200

    CSV reader
    splits the line by comma
    
    Written by sgay

--- Bison.CLI/Program.cs
+++ Bison.CLI/Program.cs
@@ -1,2 +1,26 @@
-﻿foreach (var arg in args)
-    Console.WriteLine(arg);
+﻿
+try{
+string Auther, Observation, timestamp;
+string filePath = Path.GetFullPath("bison_observe_cli_db.csv");
+//string filePath = @"C:\Users\Bruger\Desktop\ITU\ToBeRenamed\Project\Bison\Bison.CLI\bison_observe_cli_db.csv";
+using(StreamReader reader = new StreamReader(File.OpenRead(filePath)))
+    {
+        string line;
+        string waste = reader.ReadLine();
+        while(!reader.EndOfStream)
+            {
+                line = reader.ReadLine();
+
+                string[] Observe = line.Split(","); 
+                Auther = Observe[0];
+                Observation = Observe[1];
+                timestamp = Observe[2];
+
+                Console.WriteLine(Auther + " & " + Observation + " & " + timestamp);
+
+            }
+    }
+    } catch(Exception e)
+{
+    Console.WriteLine("EEEEEEEEEEEEEEEE");
+}
