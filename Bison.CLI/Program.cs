8f42d3f4f05b93a9efe52e583dd120229c7ca131
Samuel Bech Gayne <sgay@itu.dk>
2026-08-28 15:38:15 +0200

    Added observation method

--- Bison.CLI/Program.cs
+++ Bison.CLI/Program.cs
@@ -11,10 +11,20 @@ static string filePath = Path.GetFullPath("bison_observe_cli_db.csv");
 //string filePath = @"C:\Users\Bruger\Desktop\ITU\ToBeRenamed\Project\Bison\Bison.CLI\bison_observe_cli_db.csv";
 static int Main(string[] args)
         {
-        string line = args[0];
+        string line = args[0].ToLowerInvariant();
         if(line == "read")
             {
                 read();
+            }
+        
+        if(line == "observe")
+            {
+                String observation = "";
+                for(int i = 1; i < args.Length; i++)
+                {
+                    observation = observation + args[i] + " ";
+                }
+                observe(observation);
             }
     return 0;
     }
@@ -48,10 +58,28 @@ static void read()
                 }
                 } catch(Exception e)
             {
-                Console.WriteLine("EEEEEEEEEEEEEEEE");
+                Console.WriteLine(e);
             }
 
 
+        }
+
+static void observe(String observation)
+        {
+            try
+            {
+                string filePath = Path.GetFullPath("bison_observe_cli_db.csv");
+            var currentTime = DateTimeOffset.Now;
+            //File.AppendAllText(filePath, Environment.UserName + ", " + observation + ", " + currentTime.ToUnixTimeSeconds());
+            using(StreamWriter sw = File.AppendText(filePath))
+            {
+                sw.WriteLine(Environment.UserName + "," + '"'+ observation + '"' + "," + currentTime.ToUnixTimeSeconds());
+            }
+            } catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            
         }
     }
 }
